d9ac819dfd12c8f722d7fcd36625940e47a96a05
Hector Bulgarini <hbulgarini@gmail.com>
2021-03-01 07:51:51 -0300

    adding hex debug

--- src/components/Remark.tsx
+++ src/components/Remark.tsx
@@ -2,8 +2,9 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 import Keyring from '@polkadot/keyring';
+import { u8aToHex } from '@polkadot/util';
 import React from 'react';
-import { Button,Container } from 'semantic-ui-react';
+import { Button, Container } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 import { useApiSourcePromiseContext } from '../contexts/ApiPromiseSourceContext';
@@ -42,17 +43,23 @@ const Remark = ({
 		const callOriginType = sourceApi.registry.createType('CallOrigin', { SourceAccount: account.addressRaw });
 
 		console.log('callOriginType', callOriginType.toString());
+		console.log('callOriginType encoded',u8aToHex(callOriginType.toU8a()));
 
 		// @ts-ignore
 		const payloadType = sourceApi.registry.createType('OutboundPayload', payload);
 
-		console.log('payloadType',payloadType.toString());
+		console.log('payloadType',u8aToHex(payloadType.toU8a()));
 
 		console.log('payload', payload);
+		// @ts-ignore
+		const messagePayloadType = sourceApi.registry.createType('MessageData', { fee:3576409240,payload:payloadType });
+		console.log('messagePayloadType', messagePayloadType.toJSON());
 
 		const bridgeMessage = sourceApi.tx
 			.bridgeMillauMessageLane
-			.sendMessage(lane_id,  payload, 3576409240);
+			.sendMessage(lane_id, payload, 3576409240);
+
+		console.log('bridgeMessage',u8aToHex(bridgeMessage.toU8a()));
 
 		const result = await bridgeMessage.signAndSend(account);
 		console.log('result',result);
