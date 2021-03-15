36ab226ff34b3fc09665ee301cbccdad0727217f
Hector Bulgarini <hbulgarini@gmail.com>
2021-03-15 13:15:22 -0300

    Fixing input parameters

--- src/util/deriveAccount.js
+++ src/util/deriveAccount.js
@@ -2,8 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-const { stringToU8a } = require('@polkadot/util');
-const { blake2AsHex, encodeAddress } = require('@polkadot/util-crypto');
+const { compactAddLength, stringToU8a } = require('@polkadot/util');
+const { blake2AsHex, encodeAddress, decodeAddress } = require('@polkadot/util-crypto');
 
 function deriveAccount(data) {
   const SS58Prefix = 42;
@@ -15,13 +15,18 @@ function deriveAccount(data) {
 console.log('rlto', stringToU8a('rlto'));
 console.log('pallet-bridge/account-derivation/account', stringToU8a('pallet-bridge/account-derivation/account'));
 
+
+
+
 deriveAccount(
-  stringToU8a('pallet-bridge/account-derivation/account') +
-    stringToU8a('rlto') +
-    '5sauUXUfPjmwxSgmb3tZ5d6yx24eZX4wWJ2JtVUBaQqFbvEU'
+[  ...compactAddLength(stringToU8a('pallet-bridge/account-derivation/account')),
+  ...stringToU8a('rlto'),
+  ...decodeAddress('5sauUXUfPjmwxSgmb3tZ5d6yx24eZX4wWJ2JtVUBaQqFbvEU')]
 );
 
 console.log('expected: 5G6Vkt2VhS8occc66oUFZCSy56i45nuYmYZ4XiWgHAJbUiPW');
+console.log('decodedAddress',decodeAddress('5sauUXUfPjmwxSgmb3tZ5d6yx24eZX4wWJ2JtVUBaQqFbvEU'));
+
 
 // pallet-bridge/account-derivation/accountrlto5sauUXUfPjmwxSgmb3tZ5d6yx24eZX4wWJ2JtVUBaQqFbvEU
 
