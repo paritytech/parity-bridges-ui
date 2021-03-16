a43cb8e027202d787fa13900ac7c3d3c02386e33
Hector Bulgarini <hbulgarini@gmail.com>
2021-03-16 15:30:05 -0300

    Keyring finished for first round of read (does not work on chain change yet)

---
+++ src/actions/keyringActions.ts
@@ -0,0 +1,10 @@
+// Copyright 2019-2020 @paritytech/bridge-ui authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+enum KeyringActions {
+  LOAD_KEYRING = 'LOAD_KEYRING',
+  SET_KEYRING = 'SET_KEYRING',
+  KEYRING_ERROR = 'KEYRING_ERROR'
+}
+export default KeyringActions;
