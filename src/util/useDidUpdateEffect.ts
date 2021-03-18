ffd4e1966719627237e30f245c032512d9cf62bb
hbulgarini <hbulgarini@gmail.com>
2021-03-18 17:49:26 -0300

    Replacing license header

--- src/util/useDidUpdateEffect.ts
+++ src/util/useDidUpdateEffect.ts
@@ -1,9 +1,5 @@
 /* eslint-disable header/header */
 
-// Copyright 2019-2020 @paritytech/bridge-ui authors & contributors
-// This software may be modified and distributed under the terms
-// of the Apache-2.0 license. See the LICENSE file for details.
-
 import { DependencyList, EffectCallback, useEffect, useRef } from 'react';
 
 /**
