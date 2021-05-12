46b2e85d3baeab7927da0af4583a20757c618d1a
Hector Bulgarini <hector@parity.io>
2021-05-12 10:09:20 -0300

    Making connections even more dynamic and ready for new chains. (#146)
    * Fixing some values and updating docs
    
    Getting rid of redundant file
    
    removing custom types
    
    trying to restablish src/configs/substrateCustomTypes
    
    removing unnecesary folder
    
    removing unnecesary folder
    
    removing commented code
    
    updating readme
    
    Adding todo
    
    renaming getConfigs function
    
    Update src/util/getConfig.ts
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>
    
    renaming getConfigs function
    
    Setting dynamic connections parametrized on .env
    
    correcting some values
    
    Fixing some other stuff
    
    Adding documentation
    
    Fixing tests
    
    * Remove redundant files.
    
    * Add missing folder, remove extra files.
    
    * Update README.md
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>
    
    * Addresing PR feedback
    
    * Fixing some missing variables
    
    * Adapting test
    
    * fixing file path
    
    * Addressing additional PR feedback
    
    * Fixing download script
    
    * Renaming env variables
    
    * Solving custom haser type
    
    Co-authored-by: Tomasz Drwięga <tomasz@parity.io>
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>

---
+++ src/configs/chainsSetup/customConfigs.js
@@ -0,0 +1,43 @@
+// Copyright 2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges UI.
+//
+// Parity Bridges UI is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Parity Bridges UI is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges UI.  If not, see <http://www.gnu.org/licenses/>.
+
+const dotenv = require('dotenv');
+const result = dotenv.config();
+
+if (result.error) {
+  throw result.error;
+}
+
+const customTypesDir = './src/configs/substrateCustomTypes';
+
+const rialtoUrl =
+  'https://raw.githubusercontent.com/paritytech/parity-bridges-common/master/deployments/types-rialto.json';
+
+const chain1Url = result.parsed.REACT_APP_CHAIN_1_CUSTOM_TYPES_URL || rialtoUrl;
+const chain2Url = result.parsed.REACT_APP_CHAIN_2_CUSTOM_TYPES_URL || rialtoUrl;
+
+const customTypes = [
+  {
+    path: `${customTypesDir}/chain1.json`,
+    url: chain1Url
+  },
+  {
+    path: `${customTypesDir}/chain2.json`,
+    url: chain2Url
+  }
+];
+
+module.exports = customTypes;
