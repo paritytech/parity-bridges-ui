9d64b8cdb2b98ccc03221cd20e713e8560446c6a
hbulgarini <hbulgarini@gmail.com>
2021-05-28 18:37:42 -0300

    Adding warning to useMountedState

--- src/hooks/useMountedState.ts
+++ src/hooks/useMountedState.ts
@@ -16,6 +16,7 @@
 
 import { useCallback, useState } from 'react';
 import { useIsMounted } from './useIsMounted';
+import logger from '../util/logger';
 
 /**
  * Like React's [useState](https://reactjs.org/docs/hooks-reference.html#usestate)
@@ -37,6 +38,8 @@ export const useMountedState = <D>(initialState: D | (() => D)) => {
     (value: D) => {
       if (isMounted()) {
         setState(value);
+      } else {
+        logger.warn('Warning: Trying to update the state of unmounted component.');
       }
     },
     [isMounted]
