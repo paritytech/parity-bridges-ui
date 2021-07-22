53ed42c9bb33069b956e9658e87f9f49ec202b88
Adam Zerella <1501560+adamzerella@users.noreply.github.com>
2021-07-23 05:56:58 +0930

    Validate SS58 prefix length without parsing an error message (#216)
    * Validate SS58 prefix length without parsing an error message
    
    * Opting for verbose variables over linting, determine account format earlier
    
    * Initial approach of empty vars is preferred
    
    * Reverting test case back
    
    * Matching the previous logic prior to this refactor
    
    Co-authored-by: Adam Zerella <adam.zerella@gmail.com>

--- src/reducers/tests/transactionReducer.test.ts
+++ src/reducers/tests/transactionReducer.test.ts
@@ -85,7 +85,7 @@ describe('transactionReducer', () => {
         ...state,
         unformattedReceiverAddress: 'tH95Ew4kVD9VcwsyXaSdC74Noe3H8o6fJfnKhZezXHKHEcs',
         addressValidationError: 'Unsupported address SS58 prefix: 8',
-        formatFound: '8'
+        formatFound: 8
       });
     });
   });
